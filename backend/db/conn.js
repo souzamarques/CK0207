804c8d859703d59b504d369ae943451e73b4508a
Daniele Cavalcante <danielecavalcantecf@gmail.com>
2022-06-27 21:18:11 -0300

    backend: fixing warnings

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function main() {
-  await mongoose.connect('mongodb+srv://getpetapp:UtGatNKySifZd1PH@cluster0.vj3f3.mongodb.net/?retryWrites=true&w=majority')
+  await mongoose.connect('')
   console.log('Conectou com Mongoose!')
 }
 
