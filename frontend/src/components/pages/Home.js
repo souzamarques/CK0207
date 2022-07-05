dc7374e2887484f102717f88ab0d46887e8df187
Daniele Cavalcante <danielecavalcantecf@gmail.com>
2022-07-05 09:36:58 -0300

    frontend: updated home page

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -32,7 +32,7 @@ function Home() {
               ></div>
               <h3>{pet.name}</h3>
               <p>
-                <span className="bold">Peso:</span> {pet.weight}kg
+              {pet.race} - {pet.sex} - {pet.age} anos
               </p>
               {pet.available ? (
                 <Link to={`/pet/${pet._id}`}>Mais detalhes</Link>
